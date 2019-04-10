b93d087c3a17e6741dbc8ef65d49022955b176c2
Anwar Lopez <anwarlo247@gmail.com>
2019-04-10 15:09:03 -0500

    added file

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,10 +1,12 @@
 USE employees;
 
 SELECT *
-FROM employees WHERE first_name IN ('Irena','Vidya','Maya');
+FROM employees WHERE first_name IN ('Irena','Vidya','Maya')
+ORDER BY last_name,first_name DESC;
 
 SELECT *
-FROM employees WHERE last_name LIKE 'E%';
+FROM employees WHERE last_name LIKE 'E%'
+ORDER BY emp_no DESC;
 
 SELECT *
 FROM employees WHERE hire_date LIKE '199%-%';
@@ -25,8 +27,8 @@ SELECT *
 FROM employees
 WHERE gender = 'M'
   AND (first_name = 'Irena'
-         OR first_name = 'Vidya'
-         OR first_name = 'Maya');
+  OR first_name = 'Vidya'
+  OR first_name = 'Maya');
 
 SELECT *
 FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
@@ -39,7 +41,8 @@ SELECT *
 FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
 
 SELECT *
-FROM employees WHERE birth_date LIKE '%-12-25' AND hire_date LIKE '199%-%';
+FROM employees WHERE birth_date LIKE '%-12-25' AND hire_date LIKE '199%-%'
+ORDER BY birth_date DESC, hire_date ASC;
 
 SELECT *
 FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
